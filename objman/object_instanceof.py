498e9e0888578202efd63e5164bddaae0ec78b47
hmtmcse <hmtm.cse@gmail.com>
2021-05-13 01:40:04 +0600

    upgrading supplier system

---
+++ objman/object_instanceof.py
@@ -0,0 +1,20 @@
+def is_it_dictionary(data):
+    return isinstance(data, dict)
+
+
+def is_it_string(data):
+    return isinstance(data, str)
+
+
+def is_it_list(data):
+    return isinstance(data, list)
+
+
+def is_it_integer(data):
+    return isinstance(data, int)
+
+
+def is_it_float(data):
+    return isinstance(data, float)
+
+
